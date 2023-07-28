451078254248beea6c7149e6ac5eb3018f5d2e65
Steven Hanson <smhanson21@gmail.com>
2023-07-27 21:59:18 -0500

    change to fahrenheit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let rootEl = $('#root');
+let apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
 let apiKey = "0cb7223d9ba0d2ecc19d62c5046e6e35";
 let submitButton = $("#submitButton");
 let cityInput = $("#cityInput");
 let cityName = "";
+let url = "";
 
-submitButton.on("click", function () {
+submitButton.on("click", FindCity)
+
+function FindCity(event) {
+    event.preventDefault();
     cityName = cityInput.val();
-    console.log(cityName);
+    url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
     CurrentForecast();
-})
-
-let url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+}
 
 function CurrentForecast() {
 
-    console.log(cityName);
-    console.log(url);
     $.ajax({
         url: url,
         method: 'GET',
