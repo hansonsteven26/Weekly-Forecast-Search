2458e7e9dac467aeac42f3d3e49a2a9565546031
Steven Hanson <smhanson21@gmail.com>
2023-07-27 23:15:56 -0500

    add folders and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# 5-Day Forecast Search
+
+## Description
+If you enter a city in the search box, you can see the forecast for the next 5 days. You can also click on recent searches to look at a city you've already searched once more.
+
+# Screenshots
+
+# Links
+[GitHub](https://github.com/hansonsteven26/Weekly-Forecast-Search)
+[Page](https://hansonsteven26.github.io/Weekly-Forecast-Search/)
---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+let apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
+let apiKey = "0cb7223d9ba0d2ecc19d62c5046e6e35";
+let submitButton = $("#submitButton");
+let cityInput = $("#cityInput");
+let cityName = "";
+let url = "";
+let today = dayjs().format('YYYY-MM-DD');
+let nextDay = (dayjs().add(1, 'day')).format("YYYY-MM-DD");
+let dayEl = $("<h2>");
+dayEl.text("Today's date: " + today);
+$("#pageTitle").append(dayEl);
+console.log(nextDay);
+
+submitButton.on("click", FindCity)
+
+function FindCity(event) {
+    event.preventDefault();
+    cityName = cityInput.val();
+    url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
+    CurrentForecast();
+}
+
+function CurrentForecast() {
+
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).then(function (response) { // instad of .then, you can do .success
+        console.log('Ajax Reponse \n-------------');
+        console.log(response);
+        console.log(response.list[1].dt_txt); // min temp at noon
+        $("#main").removeClass("hidden");
+        
+        $.each(response.list, function () {
+            otherDays = this.dt_txt.split(" ");
+            // console.log(otherDays);
+            if (otherDays[1] == "12:00:00" && otherDays[0] != today) {
+                console.log(otherDays[0]);
+                console.log("Big print");
+            }
+            // for (let i = 0; i < 6; i++) {
+            //     yyMMDD = response.list[i].dt_txt.split(" ")[0];
+            //     console.log(yyMMDD);
+            // }
+        })
+    });
+}
