e2d2b8dc66963a692776c6dc27e3390c39ad30ec
Steven Hanson <smhanson21@gmail.com>
2023-07-28 05:26:33 -0500

    add cards. finish getting API info

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,12 @@ let cityInput = $("#cityInput");
 let cityName = "";
 let url = "";
 let today = dayjs().format('YYYY-MM-DD');
-let nextDay = (dayjs().add(1, 'day')).format("YYYY-MM-DD");
 let dayEl = $("<h2>");
+let otherDaysArray = [];
+let tempsArray = [];
+let weatherTypeArray = [];
 dayEl.text("Today's date: " + today);
 $("#pageTitle").append(dayEl);
-console.log(nextDay);
 
 submitButton.on("click", FindCity)
 
@@ -21,27 +22,47 @@ function FindCity(event) {
 }
 
 function CurrentForecast() {
-
     $.ajax({
         url: url,
         method: 'GET',
     }).then(function (response) { // instad of .then, you can do .success
         console.log('Ajax Reponse \n-------------');
         console.log(response);
-        console.log(response.list[1].dt_txt); // min temp at noon
         $("#main").removeClass("hidden");
         localStorage.setItem("recentlySearched", cityName);
         $.each(response.list, function () {
-            otherDays = this.dt_txt.split(" ");
-            // console.log(otherDays);
-            if (otherDays[1] == "12:00:00" && otherDays[0] != today) {
-                console.log(otherDays[0]);
-                console.log("Big print");
+            let otherDays = this.dt_txt.split(" ");
+            let temp = this.main.temp;
+            let weatherType = this.weather[0].main;
+            if (otherDays[0] != today && otherDays[1] == "00:00:00") {
+                otherDaysArray.push(otherDays[0]);
+                tempsArray.push(temp);
+                if (weatherType == "Clouds") {
+                    weatherType = "Cloudy";
+                }
+                weatherTypeArray.push(weatherType);
             }
-            // for (let i = 0; i < 6; i++) {
-            //     yyMMDD = response.list[i].dt_txt.split(" ")[0];
-            //     console.log(yyMMDD);
-            // }
+            // somehow get the forecast at noon for next 5 days
+            // get the weather temp and store it in html id
         })
+        while (otherDaysArray.length > 5) {
+            otherDaysArray.pop();
+        }
+        while (tempsArray.length > 5) {
+            tempsArray.pop();
+        }
+        while (weatherTypeArray.length > 5) {
+            weatherTypeArray.pop();
+        }
+        console.log(otherDaysArray);
+        console.log(tempsArray);
+        console.log(weatherTypeArray);
+
+        $("#day1").text(otherDaysArray[0]);
+        $("#day2").text(otherDaysArray[1]);
+        $("#day3").text(otherDaysArray[2]);
+        $("#day4").text(otherDaysArray[3]);
+        $("#day5").text(otherDaysArray[4]);
+
     });
 }
