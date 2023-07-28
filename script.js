702d7dcec70e58dc6d62409752a274ec79c5f058
Steven Hanson <smhanson21@gmail.com>
2023-07-27 21:28:24 -0500

    add ajax

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-let rootEl = $('#root')
+let rootEl = $('#root');
+let apiKey = "0cb7223d9ba0d2ecc19d62c5046e6e35";
+let submitButton = $("#submitButton");
+let cityInput = $("#cityInput");
+let cityName = "";
+
+submitButton.on("click", function () {
+    cityName = cityInput.val();
+    console.log(cityName);
+    CurrentForecast();
+})
+
+let url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+
+function CurrentForecast() {
+
+    console.log(cityName);
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).then(function (response) { // instad of .then, you can do .success
+        console.log('Ajax Reponse \n-------------');
+        console.log(response);
+        console.log(url);
+    });
+}
